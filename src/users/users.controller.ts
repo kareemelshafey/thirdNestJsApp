a0f4e80cd86e0f77fdf5b165ee8fd381a08c39d6
Kareem Elshafey <kareemkimo39@gmail.com>
2021-12-26 16:24:54 +0200

    passwords are now handled correctly

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Get, Delete, Patch, Param, Query, NotFoundExcep
 import { createUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor'
 
 @Controller('auth')
 export class UsersController {
@@ -13,9 +14,10 @@ export class UsersController {
     }
 
     // is is defined as a string not  a number as it is a part of the url and every part of the url is defined as a string
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string){
+        console.log('handler is running')
         const user = await this.usersService.findOne(parseInt(id));
         if(!user) {
             throw new NotFoundException('user not found')
